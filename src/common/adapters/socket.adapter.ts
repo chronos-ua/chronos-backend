d478962341f625678a3d0d5b4cb1d58c339de994
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-11-22 17:40:16 +0200

    feat: UwsSocketIoAdapter

---
+++ src/common/adapters/socket.adapter.ts
@@ -0,0 +1,31 @@
+import { IoAdapter } from "@nestjs/platform-socket.io";
+import { ServerOptions, Server } from "socket.io";
+import { App, TemplatedApp } from "uWebSockets.js";
+
+export class UwsSocketIoAdapter extends IoAdapter {
+  private readonly wsPort = 3001;
+
+  createIOServer(port: number, options?: ServerOptions): Server {
+    const uwsApp: TemplatedApp = App();
+
+    const server = super.createIOServer(this.wsPort, {
+      ...options,
+      cors: {
+        origin: "*", // TODO: Configure for prod!
+        methods: ["GET", "POST"]
+      }
+    });
+
+    server.attachApp(uwsApp);
+
+    uwsApp.listen(this.wsPort, (token) => {
+      if (token) {
+        console.log(`uWebSockets is listening on port ${this.wsPort}`);
+      } else {
+        console.error(`Failed to listen on port ${this.wsPort}`);
+      }
+    });
+
+    return server;
+  }
+}
