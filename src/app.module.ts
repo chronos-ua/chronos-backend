170a4d7178be3a148425dc9b227dee86ec857ec3
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-11-24 16:24:34 +0200

    feat(weather): crud draft

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { AuthModule } from "./modules/auth/auth.module";
+import { AuthModule } from "@thallesp/nestjs-better-auth";
+import { auth } from "./modules/auth/auth.instance";
+import { WeatherModule } from "./modules/weather/weather.module";
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule.forRoot({ auth }), WeatherModule],
   controllers: [AppController],
   providers: [AppService]
 })
