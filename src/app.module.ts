786f2318812d403b3b6febe86d88bf17c83e744c
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-11-25 20:43:37 +0200

    init calendar module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { AuthModule } from "@thallesp/nestjs-better-auth";
 import { auth } from "./modules/auth/auth.instance";
 import { WeatherModule } from "./modules/weather/weather.module";
 import { RedisModule } from "./common/redis/redis.module";
+import { CalendarModule } from "./modules/calendar/calendar.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [AuthModule.forRoot({ auth }), WeatherModule, RedisModule],
+  imports: [
+    MongooseModule.forRoot("mongodb://localhost/nest", {}),
+    AuthModule.forRoot({ auth }),
+    WeatherModule,
+    RedisModule,
+    CalendarModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
