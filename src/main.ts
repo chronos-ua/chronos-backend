3040b2932f86e333bd3700d980c0a527616daae5
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-11-29 00:48:55 +0200

    nothing strange happened, believe me

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
 import { NestFactory } from "@nestjs/core";
-import {
-  FastifyAdapter,
-  NestFastifyApplication
-} from "@nestjs/platform-fastify";
 import { AppModule } from "./app.module";
 import { UwsSocketIoAdapter } from "./common/adapters/socket.adapter";
+import {
+  ExpressAdapter,
+  NestExpressApplication
+} from "@nestjs/platform-express";
+import express from "ultimate-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
+  const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new ExpressAdapter(express()),
     { bodyParser: false }
   );
-
-  app.useWebSocketAdapter(new UwsSocketIoAdapter(app));
 
   // TODO: cors?
   app.enableCors({
     origin: "*"
   });
+
+  app.useWebSocketAdapter(new UwsSocketIoAdapter(app));
+
   const port = process.env.HTTP_PORT || 3000;
 
   await app.listen(port, "0.0.0.0", () => {
