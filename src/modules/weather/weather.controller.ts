d6379f45fbdf208752a432aea040649af2d00e6f
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-11-30 19:42:20 +0200

    feat: add DevOnly decorator to allow anonymous access for weather endpoint

--- src/modules/weather/weather.controller.ts
+++ src/modules/weather/weather.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { WeatherService } from "./weather.service";
+import { AllowAnonymous } from "@thallesp/nestjs-better-auth";
+import { DevOnly } from "src/common/decorators/devOnly.decorator";
 
 // TODO: geocoding
 // https://developers.google.com/maps/documentation/geocoding/overview
@@ -8,6 +10,7 @@ export class WeatherController {
   constructor(private readonly weatherService: WeatherService) {}
 
   @Get(":city")
+  @DevOnly(AllowAnonymous())
   getWeather(@Param("city") city: string) {
     return this.weatherService.getWeather(city);
   }
