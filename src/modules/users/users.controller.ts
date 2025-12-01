1def6c993525ac3b6ad65cf19ae572a6e56a44ac
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-01 19:33:59 +0200

    refactor: comment out findOne method implementation in UsersController

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -12,7 +12,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   @Get(":id")
   findOne(@Param("id") id: string) {
-    return this.usersService.findOne(+id);
+    // return this.usersService.findOne(+id);
   }
 
   @Get("/settings")
