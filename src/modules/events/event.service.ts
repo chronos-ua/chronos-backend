cc2e71dff682a89209a9c78bffc61e89c8c06fc0
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-02 16:36:19 +0200

    refactor: integrate Mongoose and update EventService constructor

--- src/modules/events/event.service.ts
+++ src/modules/events/event.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from "@nestjs/common";
 import { CreateEventDto } from "./dto/create-event.dto";
 import { UpdateEventDto } from "./dto/update-event.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Event } from "./schemas/ event.schema";
 
 @Injectable()
 export class EventService {
+  constructor(@InjectModel("Event") private eventModel: Model<Event>) {}
   create(createEventDto: CreateEventDto) {
     return "This action adds a new event";
   }
