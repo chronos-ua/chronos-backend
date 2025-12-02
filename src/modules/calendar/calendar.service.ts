c2df125d8987070110982c2ef2f7694d332a8658
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-02 16:36:41 +0200

    refactor: add Mongoose integration to CalendarService

--- src/modules/calendar/calendar.service.ts
+++ src/modules/calendar/calendar.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from "@nestjs/common";
 import { CreateCalendarDto } from "./dto/create-calendar.dto";
 import { UpdateCalendarDto } from "./dto/update-calendar.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Calendar } from "./schemas/ calendar.schema";
 
 @Injectable()
 export class CalendarService {
+  constructor(
+    @InjectModel("Calendar") private calendarModel: Model<Calendar>
+  ) {}
   create(createCalendarDto: CreateCalendarDto) {
     return "This action adds a new calendar";
   }
