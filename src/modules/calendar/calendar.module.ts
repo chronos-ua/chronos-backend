596033a4c83661397bbcb7beda938b5031a0cbb0
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-02 16:35:20 +0200

    refactor: add Mongoose integration and Calendar schema to Calendar module

--- src/modules/calendar/calendar.module.ts
+++ src/modules/calendar/calendar.module.ts
@@ -1,8 +1,13 @@
 import { Module } from "@nestjs/common";
 import { CalendarService } from "./calendar.service";
 import { CalendarController } from "./calendar.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { CalendarSchema } from "./schemas/ calendar.schema";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: "Calendar", schema: CalendarSchema }])
+  ],
   controllers: [CalendarController],
   providers: [CalendarService]
 })
