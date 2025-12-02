7030f0da2814fce2bc44cecea4f3877c1855fc18
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-02 23:09:56 +0200

    refactor: implement CreateCalendarDto with validation rules

--- src/modules/calendar/dto/create-calendar.dto.ts
+++ src/modules/calendar/dto/create-calendar.dto.ts
@@ -1,1 +1,26 @@
-export class CreateCalendarDto {}
+import {
+  IsBoolean,
+  IsHexColor,
+  IsOptional,
+  IsString,
+  Length
+} from "class-validator";
+
+export class CreateCalendarDto {
+  @Length(1, 69)
+  @IsString()
+  title: string;
+
+  @Length(1, 255)
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @IsHexColor()
+  @IsOptional()
+  color: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isPrivate: boolean;
+}
