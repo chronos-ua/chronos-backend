47bb5a65b34e0f4df76762625e3f91f702d6539f
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-05 14:51:52 +0200

    feat: implement CreateEventDto with validation and transformation rules

--- src/modules/events/dto/create-event.dto.ts
+++ src/modules/events/dto/create-event.dto.ts
@@ -1,1 +1,100 @@
-export class CreateEventDto {}
+import { Transform } from "class-transformer";
+import {
+  IsBoolean,
+  IsDate,
+  IsEnum,
+  IsOptional,
+  IsString,
+  Length
+} from "class-validator";
+import { EEventType, EReminderMethod } from "../schemas/ event.schema";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateEventDto {
+  @ApiProperty()
+  @Length(24, 24)
+  @Transform(({ value }) => value.trim())
+  @IsString()
+  calendarId: string;
+
+  @ApiProperty()
+  @Length(1, 255)
+  @Transform(({ value }) => value.trim())
+  @IsString()
+  @IsOptional()
+  customId?: string;
+
+  @ApiProperty()
+  @Length(1, 255)
+  @Transform(({ value }) => value.trim())
+  @IsString()
+  title: string;
+
+  @ApiProperty()
+  @Length(0, 1024)
+  @Transform(({ value }) => value.trim())
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @ApiProperty()
+  @IsEnum(EEventType)
+  @IsString()
+  @IsOptional()
+  type: EEventType = EEventType.ARRANGEMENT;
+
+  @ApiProperty()
+  @IsDate()
+  start: Date;
+
+  @ApiProperty()
+  @IsDate()
+  end: Date;
+
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  isAllDay: boolean = false;
+
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  isOutdoor: boolean = false;
+
+  @ApiProperty()
+  @IsBoolean()
+  @IsOptional()
+  isCompleted: boolean = false;
+
+  @ApiProperty()
+  @Length(0, 512)
+  @Transform(({ value }) => value.trim())
+  @IsString()
+  @IsOptional()
+  address?: string;
+
+  @ApiProperty()
+  @Length(0, 2048)
+  @Transform(({ value }) => value.trim())
+  @IsString()
+  @IsOptional()
+  externalUrl?: string;
+
+  @ApiProperty()
+  @Length(0, 2048)
+  @Transform(({ value }) => value.trim())
+  @IsString()
+  @IsOptional()
+  imageUrl?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  reminders?: Array<{ method: EReminderMethod; minutesBefore: number }>;
+
+  @ApiProperty()
+  @Length(0, 255)
+  @Transform(({ value }) => value.trim())
+  @IsString()
+  @IsOptional()
+  googleEventId?: string;
+}
