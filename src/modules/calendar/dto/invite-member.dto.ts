1e3da05b28b5471a77ab6a5c43842eb1bbfda873
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-05 03:33:52 +0200

    fix: add ApiProperty decorators to InviteMemberDto for Swagger documentation

--- src/modules/calendar/dto/invite-member.dto.ts
+++ src/modules/calendar/dto/invite-member.dto.ts
@@ -1,12 +1,15 @@
 import { IsEmail, IsEnum, IsString, Length } from "class-validator";
 import { ECalendarRole } from "../schemas/calendar.schema";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class InviteMemberDto {
+  @ApiProperty()
   @IsEmail()
   @Length(5, 255)
   @IsString()
   email: string;
 
+  @ApiProperty({ enum: ECalendarRole })
   @IsEnum(ECalendarRole)
   @IsString()
   role: ECalendarRole;
