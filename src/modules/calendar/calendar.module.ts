e421adba5a1168fc0691759d3371ec04373f8673
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-05 03:31:07 +0200

    fix: correct import path for CalendarSchema in calendar.module.ts

--- src/modules/calendar/calendar.module.ts
+++ src/modules/calendar/calendar.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { CalendarService } from "./calendar.service";
 import { CalendarController } from "./calendar.controller";
 import { MongooseModule } from "@nestjs/mongoose";
-import { CalendarSchema } from "./schemas/ calendar.schema";
+import { CalendarSchema } from "./schemas/calendar.schema";
 
 @Module({
   imports: [
