4de351ee4b0a60be4fc4d488710beb2ff346f9f8
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-05 07:48:53 +0200

    feat: extend GetMessagesResponseDto with additional properties for message details

--- src/modules/chat/dto/get-messages.dto.ts
+++ src/modules/chat/dto/get-messages.dto.ts
@@ -15,3 +15,29 @@ export class GetMessagesDto {
   @IsString()
   contextType: EChatContext;
 }
+
+export class GetMessagesResponseDto {
+  @ApiProperty({ type: String })
+  id: string;
+
+  @ApiProperty({ type: String })
+  contextId: string;
+
+  @ApiProperty({ enum: EChatContext })
+  contextType: EChatContext;
+
+  @ApiProperty({ type: String })
+  senderId: string;
+
+  @ApiProperty({ type: String })
+  content: string;
+
+  @ApiProperty({ type: [String] })
+  readBy: string[];
+
+  @ApiProperty({ type: String, format: "date-time" })
+  createdAt: Date;
+
+  @ApiProperty({ type: String, format: "date-time" })
+  updatedAt: Date;
+}
