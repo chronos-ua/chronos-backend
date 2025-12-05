1ec5678cef2203e3cefbf4b402ec3271b9a4f07d
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-05 15:20:25 +0200

    fix: restrict changing event creatorId

--- src/modules/events/dto/update-event.dto.ts
+++ src/modules/events/dto/update-event.dto.ts
@@ -1,10 +1,12 @@
-import { PartialType } from "@nestjs/mapped-types";
+import { OmitType, PartialType } from "@nestjs/mapped-types";
 import { CreateEventDto } from "./create-event.dto";
 import { ApiProperty } from "@nestjs/swagger";
 import { IsString, Length } from "class-validator";
 import { Transform } from "class-transformer";
 
-export class UpdateEventDto extends PartialType(CreateEventDto) {
+export class UpdateEventDto extends PartialType(
+  OmitType(CreateEventDto, ["creatorId"] as const)
+) {
   @ApiProperty()
   @Length(24, 24)
   @Transform(({ value }) => value.trim())
