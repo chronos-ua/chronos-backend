0f630ea67e52065882033d28980d9c6551dde2f0
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-05 07:08:57 +0200

    feat: add Mongoose integration for ChatMessage schema in chat module

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -3,9 +3,16 @@ import { ChatController } from "./chat.controller";
 import { ChatService } from "./chat.service";
 import { CalendarModule } from "../calendar/calendar.module";
 import { ChatGateway } from "./chat.gateway";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ChatMessageSchema } from "./schemas/chatMessage.schema";
 
 @Module({
-  imports: [CalendarModule],
+  imports: [
+    CalendarModule,
+    MongooseModule.forFeature([
+      { name: "ChatMessage", schema: ChatMessageSchema }
+    ])
+  ],
   controllers: [ChatController],
   providers: [ChatService, ChatGateway],
   exports: []
