12f096b72a8be37c6a1fd266ba29e6aa77008ed6
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-05 07:32:55 +0200

    feat: implement getMessages endpoint in ChatController

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -1,6 +1,23 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Session } from "@nestjs/common";
+import { ApiOperation } from "@nestjs/swagger";
+import { ChatService } from "./chat.service";
+import type { IUserSession } from "../auth/auth.interfaces";
+import { GetMessagesDto } from "./dto/get-messages.dto";
 
 @Controller("chat")
 export class ChatController {
-  //
+  constructor(private readonly chatService: ChatService) {}
+
+  @ApiOperation({ summary: "Get chat messages" })
+  @Get("messages")
+  async getMessages(
+    @Session() session: IUserSession,
+    @Body() dto: GetMessagesDto
+  ) {
+    return await this.chatService.getMessagesByContext(
+      dto.contextId,
+      dto.contextType,
+      session.user.id
+    );
+  }
 }
