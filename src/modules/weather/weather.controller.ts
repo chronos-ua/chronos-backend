e21bffc05c6e2a2f7bb2260feb9eb4d9527f7620
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-05 08:30:48 +0200

    feat: add ApiOkResponse decorators to getWeather and getWeatherRoot methods for improved API documentation

--- src/modules/weather/weather.controller.ts
+++ src/modules/weather/weather.controller.ts
@@ -4,6 +4,7 @@ import { AllowAnonymous, Session } from "@thallesp/nestjs-better-auth";
 import { DevOnly } from "src/common/decorators/devOnly.decorator";
 import { type Request } from "express";
 import type { IUserSession } from "../auth/auth.interfaces";
+import { ApiOkResponse } from "@nestjs/swagger";
 
 // TODO: geocoding
 // https://developers.google.com/maps/documentation/geocoding/overview
@@ -13,12 +14,20 @@ export class WeatherController {
 
   @Get(":city")
   @DevOnly(AllowAnonymous())
+  @ApiOkResponse({
+    description: "Weather information for the specified city",
+    type: Object
+  })
   getWeather(@Param("city") city: string) {
     return this.weatherService.getWeather(city);
   }
 
   @Get()
   @DevOnly(AllowAnonymous())
+  @ApiOkResponse({
+    description: "Weather information for the user's city or by IP",
+    type: Object
+  })
   getWeatherRoot(@Req() req: Request, @Session() session: IUserSession | null) {
     const city = session?.user?.city;
     let ip = req.ip;
