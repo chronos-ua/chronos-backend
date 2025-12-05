536b4bf23d728a4bf81c33e793cea861dd56e869
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-05 03:58:37 +0200

    fix: add missing UsersModule import and correct exports in CalendarModule and UsersModule

--- src/modules/calendar/calendar.module.ts
+++ src/modules/calendar/calendar.module.ts
@@ -3,10 +3,12 @@ import { CalendarService } from "./calendar.service";
 import { CalendarController } from "./calendar.controller";
 import { MongooseModule } from "@nestjs/mongoose";
 import { CalendarSchema } from "./schemas/calendar.schema";
+import { UsersModule } from "../users/users.module";
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: "Calendar", schema: CalendarSchema }])
+    MongooseModule.forFeature([{ name: "Calendar", schema: CalendarSchema }]),
+    UsersModule
   ],
   controllers: [CalendarController],
   providers: [CalendarService],
