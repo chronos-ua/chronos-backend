d20e202b34fef4fc79a8f15518b9687c38d0099d
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-05 07:48:57 +0200

    feat: add ApiOkResponse decorator to getMessages method for improved API documentation

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -1,14 +1,19 @@
 import { Body, Controller, Get, Session } from "@nestjs/common";
-import { ApiOperation } from "@nestjs/swagger";
+import { ApiOkResponse, ApiOperation } from "@nestjs/swagger";
 import { ChatService } from "./chat.service";
 import type { IUserSession } from "../auth/auth.interfaces";
-import { GetMessagesDto } from "./dto/get-messages.dto";
+import { GetMessagesDto, GetMessagesResponseDto } from "./dto/get-messages.dto";
 
 @Controller("chat")
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
   @ApiOperation({ summary: "Get chat messages" })
+  @ApiOkResponse({
+    description: "List of chat messages",
+    type: GetMessagesResponseDto,
+    isArray: true
+  })
   @Get("messages")
   async getMessages(
     @Session() session: IUserSession,
