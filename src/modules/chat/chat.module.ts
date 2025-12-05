17475b30190ed442f8933ea47d369349781d30ba
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-05 04:53:42 +0200

    feat: add ChatGateway to providers in ChatModule

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { ChatController } from "./chat.controller";
 import { ChatService } from "./chat.service";
 import { CalendarModule } from "../calendar/calendar.module";
+import { ChatGateway } from "./chat.gateway";
 
 @Module({
   imports: [CalendarModule],
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [ChatService, ChatGateway],
   exports: []
 })
 export class ChatModule {}
