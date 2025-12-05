374073d0b607a61daf2441d812f9244302a52ae3
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-05 06:08:30 +0200

    init base chat gateway

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -1,9 +1,47 @@
-import { SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { Logger } from "@nestjs/common";
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer
+} from "@nestjs/websockets";
+import { Socket, Server } from "socket.io";
+import { DevOnly } from "src/common/decorators/devOnly.decorator";
 
-@WebSocketGateway()
-export class ChatGateway {
-  @SubscribeMessage("message")
-  handleMessage(client: any, payload: any): string {
-    return "Hello world!";
+@WebSocketGateway({
+  cors: {
+    origin: "*",
+    credentials: true
+  },
+  namespace: "chat"
+})
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private readonly logger = new Logger(ChatGateway.name);
+
+  afterInit(server: Server) {
+    this.logger.log("ChatGateway initialized");
+  }
+
+  handleConnection(@ConnectedSocket() client: Socket) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(@ConnectedSocket() client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage("echo")
+  handleMessage(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: any
+  ): string {
+    this.logger.log(`Received message: ${data}`);
+    return data;
   }
 }
