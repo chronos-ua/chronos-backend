700ea88826cdfb2ef3d659eb481d2873617e3bbc
Vlad Pavlenko <pavlenkoliar@gmail.com>
2025-12-06 12:48:10 +0200

    oopsie, return calendarId

--- src/modules/events/dto/create-event.dto.ts
+++ src/modules/events/dto/create-event.dto.ts
@@ -12,6 +12,12 @@ import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateEventDto {
   @ApiProperty()
+  @Length(24, 24)
+  @Transform(({ value }) => value.trim())
+  @IsString()
+  calendarId: string;
+
+  @ApiProperty()
   @Length(1, 255)
   @Transform(({ value }) => value.trim())
   @IsString()
